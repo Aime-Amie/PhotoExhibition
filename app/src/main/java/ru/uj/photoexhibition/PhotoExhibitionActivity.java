d775974f67cb37e85fe2d3ea086399b730e18c8d
EvgenyBlokhin <zhekablohin1@gmail.com>
2018-11-05 15:15:16 +0700

    no message

---
+++ app/src/main/java/ru/uj/photoexhibition/PhotoExhibitionActivity.java
@@ -0,0 +1,13 @@
+package ru.uj.photoexhibition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoExhibitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_exhibition);
+    }
+}
